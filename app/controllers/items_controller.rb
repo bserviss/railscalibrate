45c8c2ca16de0e006a9ae8af5c3cb3c92e4c6517
B Serviss <thebserviss@gmail.com>
2010-09-12 11:26:55 -0400

    fixed a bug with the navbar - added controller to links as without it the current controller is used

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,8 +7,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.xml
   def index
-    @items = Item.due_cal.paginate :page => params[:page], :per_page => params[:per_page]
-
+    @items = Item.due_cal.paginate :page => params[:page], :per_page => 10
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @items }
@@ -158,7 +157,6 @@ class ItemsController < ApplicationController
   end
 
   def show_in_cal
-    @display_group = Item.in_cal
     @items = Item.in_cal.paginate :page => params[:page], :per_page => params[:per_page]
   end
 
