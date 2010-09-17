c64d1be0a9aa64249d5aaf41deea764f1ea376e1
B Serviss <thebserviss@gmail.com>
2010-09-16 22:14:40 -0400

    added last cal event to item show view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @issues = @item.issues.all(:order => 'created_at DESC')
     @dependents = @item.dependents.all( :order => 'created_at DESC')
+    @event = @item.events.first( :order => 'created_at DESC' )
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @item }
