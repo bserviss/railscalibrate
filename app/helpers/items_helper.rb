48ddcd1d915ed2ea464c9e44de727e433c392e2c
B Serviss <thebserviss@gmail.com>
2012-08-04 18:04:26 -0400

    added helper to simplify the display of sn and due cal blurb

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,6 @@
 module ItemsHelper
+  def show_sn_and_last_cal( item )
+    raw( "The Serial Number is #{ item.org_sn } " +
+      "and it was due cal on #{ h (item.last_calibrated_on + item.cal_cycle_days.days).strftime('%m/%d/%Y')}" )
+  end
 end
